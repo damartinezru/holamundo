8484151c7429bc4f399c9c40627dcedb64300614
damartinezru <damartinezru@gmail.com>
2018-02-08 07:13:19 -0500

    puse el string

--- HolaMundo/src/holamundo/HolaMundo.java
+++ HolaMundo/src/holamundo/HolaMundo.java
@@ -5,6 +5,8 @@
  */
 package holamundo;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Estudiantes
@@ -15,7 +17,12 @@ public class HolaMundo {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+       
+        String nombre;
+        Scanner sc=new Scanner(System.in);
+        nombre=sc.next();
+        System.out.println("Hola mundo "+nombre);
+        
     }
     
 }
